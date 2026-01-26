c318241407679d5a10f5c93505901cdd77d349de
Programmer Tanish <157118290+ProgrammerTanish@users.noreply.github.com>
2026-01-25 19:23:36 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_CATEGORIES = {
+    "Images": ["jpg", "jpeg", "png", "gif", "bmp"],
+    "Documents": ["pdf", "docx", "txt", "pptx", "xlsx"],
+    "Videos": ["mp4", "mkv", "avi"],
+    "Audio": ["mp3", "wav"],
+    "Archives": ["zip", "rar", "7z"]
+}
